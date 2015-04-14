6cb1fc4b41b19351da1f988b6d2ec80e32adca48
David Eads <davideads@gmail.com>
2015-04-14 00:54:20 -0400

    deckjs lol

--- app/app.js
+++ app/app.js
@@ -1,24 +1,19 @@
-var $wrapper;
+var $slideContainer;
 var $slides;
 
-var afterSlideLoad = function(anchorLink, index, slideAnchor, slideIndex) {
-  loadImages(slideIndex);
-  $slides.eq(slideIndex).addClass('active');
+var onSlideChange = function(e, from, to) {
+    loadImages(to);
 }
 
-var onSlideLeave = function(anchorLink, index, slideIndex, direction) {
-  $slides.eq(slideIndex).removeClass('active');
-}
-
-var loadImages = function(slideIndex) {
+var loadImages = function(index) {
     var slides = [
-      $slides.eq(slideIndex - 1),
-      $slides.eq(slideIndex),
-      $slides.eq(slideIndex + 1),
+        $slides.eq(index),
+        $slides.eq(index + 1),
+        $slides.eq(index - 1),
     ];
 
     for (var i = 0; i < slides.length; i++) {
-      loadImage(slides[i]);
+        loadImage(slides[i]);
     };
 }
 
@@ -27,34 +22,21 @@ var loadImage = function($slide) {
     * Lazy load images.
     */
     var background_image = $slide.data('background-image');
-
-    if ($slide.css('background-image') == 'none' && background_image) {
-      $slide.css('background-image', 'url(' + background_image + ')');
-    }
+    if ($slide.css('background-image') == 'none' && background_image)
+        $slide.css('background-image', 'url(' + background_image + ')');
 
     var background_size = $slide.data('background-size');
     if (background_size)
-      $slide.css('background-size', background_size);
+        $slide.css('background-size', background_size);
 
 };
 
 $(document).ready(function() {
-  $wrapper = $('#fullpage');
+  $container = $('.deck-container');
   $slides = $('.slide');
 
-  var anchors = ['_'];
-  $slides.each(function() {
-    anchors.push($(this).data('anchor'));
-  });
+  $(this).bind('deck.change', onSlideChange);
 
-
-  $wrapper.fullpage({
-    anchors: (TARBELL_PREVIEW_SERVER) ? anchors : false,
-    loopHorizontal: false,
-    scrollingSpeed: 400,
-    animateAnchor: false,
-    onSlideLeave: onSlideLeave,
-    afterSlideLoad: afterSlideLoad
-  });
-
+  $.deck($slides);
+  $container.removeClass('hidden');
 });
